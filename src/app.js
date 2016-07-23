a96a22a45c83606c19eb3a23ccaac29b04f4946c
Karol Altamirano <karol.altamirano@unit9.com>
2016-07-23 10:01:57 +0200

    Setup express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
+import routes from './routes/index';
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routes);
+
+export default app;
--- src/bin/main.js
+++ src/bin/main.js
@@ -1,3 +1,21 @@
 import 'babel-polyfill';
 
-console.log('Hello World');
+import debug from 'debug';
+import http from 'http';
+
+import app from '../app';
+
+app.set('port', process.env.PORT || 5000);
+
+var log = debug('myapp:server');
+
+var server = http.createServer(app);
+
+server.on('listening', () => {
+    var addr = server.address(),
+        bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+
+    log('Listening on ' + bind);
+});
+
+server.listen(app.get('port'));
