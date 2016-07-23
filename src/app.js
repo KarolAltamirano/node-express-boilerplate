66d6b92e6f4af05cb2bbd60b392763715f47caf7
Karol Altamirano <karol.altamirano@unit9.com>
2016-07-24 00:38:39 +0200

    Express app code comments

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ import bodyParser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+// import routes
 import apiTest from './routes/apiTest';
 import apiNotExists from './routes/apiNotExists';
 
+// create express app
 var app = express();
 
+// add middlewares
 app.use(compression());
 app.use(cors());
 app.use(morgan('dev'));
@@ -20,9 +23,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// add API routes
 app.use('/api', apiTest);
 app.use('/api', apiNotExists);
 
+// add static middleware to serve static files
 app.use(history({ verbose: true, logger: debug('myapp:history') }));
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
