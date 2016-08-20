3f67e0028de832941749a293e4cb1012373d56bb
Karol Altamirano <karol.altamirano@unit9.com>
2016-08-20 02:10:19 +0200

    Configure eslint

--- src/routes/apiNotExists.js
+++ src/routes/apiNotExists.js
@@ -1,9 +1,11 @@
+// @flow
+
 import { Router } from 'express';
 
 var router = Router();
 
 // fallback for not existing API
-router.use((req, res) => {
+router.use((req: any, res: any) => {
     res.status(500);
     res.send('API does not exist');
 });
