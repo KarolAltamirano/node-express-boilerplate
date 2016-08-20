fcb191dee600222817e1f12a25f4081d9c8c6b94
Karol Altamirano <karol.altamirano@unit9.com>
2016-08-20 02:00:09 +0200

    Configure eslint flow

--- src/routes/apiTest.js
+++ src/routes/apiTest.js
@@ -1,9 +1,11 @@
+// @flow
+
 import { Router } from 'express';
 
 var router = Router();
 
 // GET test API
-router.get('/test', (req, res) => {
+router.get('/test', (req: any, res: any) => {
     res.send('Hello from test API');
 });
 
