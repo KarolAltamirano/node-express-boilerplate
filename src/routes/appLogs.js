3784e9387bd5ae66dad4d2bc7700ec1b6a8a4a0b
Karol Altamirano <kaltamirano@noon.com>
2017-02-26 13:04:15 +0400

    Create logging middlewares

---
+++ src/routes/appLogs.js
@@ -0,0 +1,44 @@
+// @flow
+
+import path from 'path';
+import express, { Router } from 'express';
+import serveIndex from 'serve-index';
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import basicauth from 'basicauth-middleware';
+import sha1 from 'sha1';
+
+// requests logger
+const logsLogger = expressWinston.logger({
+    transports: [new winston.transports.File({
+        filename: './logs/logs-logger.log',
+        maxsize: 5000000,
+        maxFiles: 5
+    })]
+});
+
+// error logger
+const logsErrorLogger = expressWinston.errorLogger({
+    transports: [new winston.transports.File({
+        filename: './logs/logs-error-logger.log',
+        maxsize: 5000000,
+        maxFiles: 5
+    })]
+});
+
+// serve logs files
+const logsServe = Router();
+
+logsServe.use('/logs',
+    basicauth((username: string, password: string): boolean => (
+        username === process.env.LOGS_USERNAME && sha1(password) === process.env.LOGS_PASSWORD
+    )),
+    express.static(path.join(__dirname, '../../', 'logs')),
+    serveIndex(path.join(__dirname, '../../', 'logs'), { icons: true })
+);
+
+export {
+    logsLogger,
+    logsErrorLogger,
+    logsServe
+};
